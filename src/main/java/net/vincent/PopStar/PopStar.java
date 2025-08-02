377fcf905b4652ea1f6cd2424cb302983b952fd2
Vincent Yang <yang.vincent.h@icloud.com>
2025-08-02 20:35:33 +0800

    commit

--- src/main/java/net/vincent/PopStar/PopStar.java
+++ src/main/java/net/vincent/PopStar/PopStar.java
@@ -1,4 +1,4 @@
-package main;
+package main.java.net.vincent.PopStar;
 
 import javax.swing.JFrame;
 
--- src/main/java/net/vincent/PopStar/PopStarFrame.java
+++ src/main/java/net/vincent/PopStar/PopStarFrame.java
@@ -1,4 +1,4 @@
-package main;
+package main.java.net.vincent.PopStar;
 
 import java.awt.Color;
 
